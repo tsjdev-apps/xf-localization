ee1d739be6095b36d8f81702193900cebd898916
Sebastian Jensen <apps@tsjdev-apps.de>
2021-07-15 22:15:19 +0200

    Add code for demonstrating the localization of an Xamarin.Forms app.

---
+++ XFLocalization/XFLocalization/XFLocalization/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.CommunityToolkit.Helpers;
+using Xamarin.Forms;
+using XFLocalization.Resources;
+
+namespace XFLocalization
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			LocalizationResourceManager.Current.Init(AppResources.ResourceManager);
+
+			MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
